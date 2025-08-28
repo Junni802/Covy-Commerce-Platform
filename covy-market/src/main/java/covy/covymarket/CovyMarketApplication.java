4ad493599dc210bde9f3e35ffef4281dee61f4e7
covy <junni802@naver.com>
2025-08-28 12:05:15 +0900

    feat: add

---
+++ covy-market/src/main/java/covy/covymarket/CovyMarketApplication.java
@@ -0,0 +1,13 @@
+package covy.covymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovyMarketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CovyMarketApplication.class, args);
+  }
+
+}
