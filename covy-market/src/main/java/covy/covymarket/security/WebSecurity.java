2ead9f2b800c8a6e42db91f8e624ae4a1c59ef31
covy <junni802@naver.com>
2025-08-29 09:40:51 +0900

    feat: add AuthenticationFilter

--- covy-market/src/main/java/covy/covymarket/security/WebSecurity.java
+++ covy-market/src/main/java/covy/covymarket/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package covy.covymarket.security;
 
+import covy.covymarket.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
