54869399f881e7c09ef1270cf2f42dda1b66d9b3
covy <junni802@naver.com>
2025-09-01 19:03:22 +0900

    feat: add ResponseOrder Class

---
+++ covy-order/src/main/java/covy/covyorder/vo/ResponseOrder.java
@@ -0,0 +1,19 @@
+package covy.covyorder.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@JsonInclude(Include.NON_NULL)
+public class ResponseOrder {
+  private String productId;
+  private Integer qty;
+  private Integer unitPrice;
+  private Integer totalPrice;
+  private Date createAt;
+
+  private String orderId;
+}
