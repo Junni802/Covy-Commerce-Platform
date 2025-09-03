aa4c44a09e10a2af14f58168635b8c650dd342bd
covy <junni802@naver.com>
2025-09-03 19:55:00 +0900

    Feat: add ResponseCatalog

---
+++ covy-goods/src/main/java/covy/covygoods/vo/ResponseCatalog.java
@@ -0,0 +1,16 @@
+package covy.covygoods.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.Data;
+
+@Data
+@JsonInclude(Include.NON_NULL)
+public class ResponseCatalog {
+
+  private String productId;
+  private String productName;
+  private Integer unitPrice;
+  private Integer totalPrice;
+  private Integer stock;
+}
