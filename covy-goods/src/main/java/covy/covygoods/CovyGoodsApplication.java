dce2375506e0d056777972fac7efd63f450c0138
covy <junni802@naver.com>
2025-09-03 17:57:20 +0900

    Feat: add Covy market goods platform

---
+++ covy-goods/src/main/java/covy/covygoods/CovyGoodsApplication.java
@@ -0,0 +1,13 @@
+package covy.covygoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovyGoodsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CovyGoodsApplication.class, args);
+  }
+
+}
