1ddf1d01bbe9803258acd08398cbcdbec2a0f583
covy <junni802@naver.com>
2025-09-11 18:22:49 +0900

    feat: add covy-goods filter

--- covy-goods/src/main/java/covy/covygoods/controller/CatalogController.java
+++ covy-goods/src/main/java/covy/covygoods/controller/CatalogController.java
@@ -11,12 +11,10 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/catalog-service")
 public class CatalogController {
 
   Environment env;
