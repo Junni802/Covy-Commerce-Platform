be84f81e948c20158b1835506ebe292a0427cd6c
covy <junni802@naver.com>
2025-09-15 19:57:24 +0900

    Feat: add

--- covy-goods/src/main/java/covy/covygoods/controller/CatalogController.java
+++ covy-goods/src/main/java/covy/covygoods/controller/CatalogController.java
@@ -11,10 +11,12 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/")
 public class CatalogController {
 
   Environment env;
