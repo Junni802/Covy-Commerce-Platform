721dac3a8ee9f930d6ef8157c0c7480f5d66f955
covy <junni802@naver.com>
2025-09-18 18:57:11 +0900

    feat: add Catalog Data

---
+++ covy-goods/src/test/java/covy/covygoods/controller/CatalogControllerTest.java
@@ -0,0 +1,29 @@
+package covy.covygoods.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import covy.covygoods.entity.CatalogEntity;
+import covy.covygoods.repository.CatalogRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogControllerTest {
+
+  @Autowired
+  private CatalogRepository catalogRepository;
+
+  @Test
+  void test() {
+    for (int i = 1; i <= 10000; i++) {
+      CatalogEntity entity = new CatalogEntity();
+      entity.setProductId("P-" + i); // 고유 productId (중복 방지)
+      entity.setProductName("초코에몽" + i);
+      entity.setStock(100); // 초기 재고 임의 값
+      entity.setUnitPrice(1500); // 임의 가격
+      catalogRepository.save(entity);
+    }
+  }
+
+}
