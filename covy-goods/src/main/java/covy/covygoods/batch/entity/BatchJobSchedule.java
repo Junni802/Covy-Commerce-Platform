96289d8666a1a494f10ed10a58583d8408c3d36d
covy <junni802@naver.com>
2025-09-26 18:15:52 +0900

    feat: add BatchJobSchedule

---
+++ covy-goods/src/main/java/covy/covygoods/batch/entity/BatchJobSchedule.java
@@ -0,0 +1,20 @@
+package covy.covygoods.batch.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "batch_job_schedule")
+@Getter
+@Setter
+public class BatchJobSchedule {
+  @Id
+  private String jobName;
+
+  private String cronExpression;
+
+  private Boolean enabled;
+}
