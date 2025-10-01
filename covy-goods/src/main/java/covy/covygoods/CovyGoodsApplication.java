2f8f25c2a67596d851e592c2f69c485d23105303
covy <junni802@naver.com>
2025-10-01 14:58:21 +0900

    Feat: add [springBatch] goodsItem -> Elasticsearch

--- covy-goods/src/main/java/covy/covygoods/CovyGoodsApplication.java
+++ covy-goods/src/main/java/covy/covygoods/CovyGoodsApplication.java
@@ -1,9 +1,16 @@
 package covy.covygoods;
 
+import covy.covygoods.batch.properties.BatchJobProperties;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BatchJobProperties.class)
+@EnableBatchProcessing
+@EnableScheduling
 public class CovyGoodsApplication {
 
   public static void main(String[] args) {
---
+++ covy-goods/src/main/java/covy/covygoods/batch/properties/BatchJobProperties.java
@@ -0,0 +1,22 @@
+package covy.covygoods.batch.properties;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "batch")
+public class BatchJobProperties {
+  private Map<String, String> jobs;
+
+  @PostConstruct
+  public void init() {
+    System.out.println("Loaded batch.jobs: " + jobs);
+  }
+}
