ef63a7b244931bde815a72a92a5bb05794bce73d
covy <junni802@naver.com>
2025-10-12 18:50:42 +0900

    feat: Add GoodsSearch Paging

--- covy-goods/src/main/java/covy/covygoods/controller/GoodsController.java
+++ covy-goods/src/main/java/covy/covygoods/controller/GoodsController.java
@@ -9,11 +9,14 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -48,8 +51,13 @@ public class GoodsController {
   }
 
   @GetMapping("/user/{goodsNm}")
-  public ResponseEntity<List<ResponseCatalog>> getFindGoods(@PathVariable(name = "goodsNm") String goodsNm) {
-    Iterable<GoodsDocument> goodsList = goodsService.getgoods(goodsNm);
+  public ResponseEntity<List<ResponseCatalog>> getFindGoods(@PathVariable(name = "goodsNm") String goodsNm,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "20") int size) {
+
+    Pageable pageable = PageRequest.of(page, size);
+    Iterable<GoodsDocument> goodsList = goodsService.getgoods(goodsNm, pageable);
+
 
     List<ResponseCatalog> resut = new ArrayList<>();
     goodsList.forEach(v -> {
