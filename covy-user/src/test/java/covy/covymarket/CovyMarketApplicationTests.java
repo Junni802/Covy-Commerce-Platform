31a817835cf645aca7461ff003b862dd6c0c4741
covy <junni802@naver.com>
2025-10-14 08:34:29 +0900

    feat: fix Project name

---
+++ covy-user/src/test/java/covy/covymarket/CovyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.covymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovyMarketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
