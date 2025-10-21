b0a6699408ceb348c1141d52c3dc5f0640e5ee3b
covy <junni802@naver.com>
2025-10-21 19:38:30 +0900

    feat:add Covy-cart Service Project

---
+++ covy-cart/src/main/java/covy/covycart/CovyCartApplication.java
@@ -0,0 +1,13 @@
+package covy.covycart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovyCartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CovyCartApplication.class, args);
+  }
+
+}
---
+++ covy-cart/src/test/java/covy/covycart/CovyCartApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.covycart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovyCartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
