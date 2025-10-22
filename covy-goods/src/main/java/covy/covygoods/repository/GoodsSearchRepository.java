0d008d41044ac14e10f5c0942cdd1b418f70f273
covy <junni802@naver.com>
2025-10-22 13:28:14 +0900

    feat:Add search GoodsCd Optional

--- covy-goods/src/main/java/covy/covygoods/repository/GoodsSearchRepository.java
+++ covy-goods/src/main/java/covy/covygoods/repository/GoodsSearchRepository.java
@@ -1,6 +1,7 @@
 package covy.covygoods.repository;
 
 import covy.covygoods.common.elastic.document.GoodsDocument;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GoodsSearchRepository extends ElasticsearchRepository<GoodsDocument, String> {
   Page<GoodsDocument> findByGoodsNmContaining(String goodsNm, Pageable pageable);
+  Optional<GoodsDocument> findByGoodsCd(String goodsCd);
 }
