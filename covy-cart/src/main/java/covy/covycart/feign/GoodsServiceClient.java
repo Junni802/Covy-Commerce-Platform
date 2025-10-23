bae4a186048fb0b03573161338f69e5de7e89fdc
covy <junni802@naver.com>
2025-10-23 18:25:24 +0900

    feat:Add GoodsServiceClient

---
+++ covy-cart/src/main/java/covy/covycart/feign/GoodsServiceClient.java
@@ -0,0 +1,16 @@
+package covy.covycart.feign;
+
+import covy.covycart.goods.dto.GoodsResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "covy-goods", url = "${covy-goods-url}")
+public interface GoodsServiceClient {
+
+  @GetMapping("/search-goodsCd/{goodsCd}")
+  GoodsResponseDto getGoodsCd(@PathVariable("goodsCd") String goodsCd);
+
+}
+
