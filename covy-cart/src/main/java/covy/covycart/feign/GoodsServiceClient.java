49dee5ae6d15811aecbb2897f8a25f1a120c0538
covy <junni802@naver.com>
2025-10-23 18:27:27 +0900

    feat:fix GoodsServiceCielnt

--- covy-cart/src/main/java/covy/covycart/feign/GoodsServiceClient.java
+++ covy-cart/src/main/java/covy/covycart/feign/GoodsServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "covy-goods", url = "${covy-goods-url}")
+@FeignClient(name = "covy-goods")
 public interface GoodsServiceClient {
 
   @GetMapping("/search-goodsCd/{goodsCd}")
