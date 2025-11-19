d63a0cdff94c93bea2afe1a862a2a64cfe006fe4
covy <junni802@naver.com>
2025-11-19 16:58:21 +0900

    refactor(user-repo): add @Repository and documentation to UserRepository

--- covy-user/src/main/java/covy/covyuser/user/repository/UserRepository.java
+++ covy-user/src/main/java/covy/covyuser/user/repository/UserRepository.java
@@ -2,10 +2,28 @@ package covy.covyuser.user.repository;
 
 import covy.covyuser.user.entitiy.UserEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * User Entity CRUD Repository
+ * - 사용자 조회 및 저장 기능 제공
+ */
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
+  /**
+   * 사용자 ID로 조회
+   *
+   * @param userId 사용자 고유 ID
+   * @return UserEntity
+   */
   UserEntity findByUserId(String userId);
 
+  /**
+   * 이메일로 조회
+   *
+   * @param username 사용자 이메일
+   * @return UserEntity
+   */
   UserEntity findByEmail(String username);
 }
