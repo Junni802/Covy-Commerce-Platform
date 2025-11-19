3febde99d801b10c453252fca53680ef9fdd6b16
covy <junni802@naver.com>
2025-11-19 16:56:46 +0900

    refactor(user-controller): streamline UserController with ModelMapper reuse and Stream API

--- covy-user/src/main/java/covy/covyuser/user/controller/UserController.java
+++ covy-user/src/main/java/covy/covyuser/user/controller/UserController.java
@@ -6,87 +6,103 @@ import covy.covyuser.user.service.UserService;
 import covy.covyuser.user.vo.RequestUser;
 import covy.covyuser.user.vo.ResponseUser;
 import io.micrometer.core.annotation.Timed;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * User 서비스 REST 컨트롤러
+ * <p>
+ * - 헬스 체크, 유저 생성, 조회 등 API 제공
+ */
 @RestController
 @RequestMapping("/")
 public class UserController {
 
-  private UserService userService;
-  private Environment env;
+  private final UserService userService;
+  private final Environment env;
+  private final ModelMapper modelMapper;
+
   @Value("${greeting.message}")
   private String greeting;
 
   public UserController(UserService userService, Environment env) {
     this.userService = userService;
     this.env = env;
+    this.modelMapper = new ModelMapper();
+    this.modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
   }
 
+  /**
+   * 서비스 헬스 체크 엔드포인트
+   */
   @GetMapping("/health_check")
   @Timed(value = "users.status", longTask = true)
   public String status() {
-    return String.format("It's Working in User Service"
-        + ", port(local.server.port)=" + env.getProperty("local.server.port")
-        + ", port(server.port)=" + env.getProperty("server.port")
-        + ", token secret=" + env.getProperty("jwt.secret")
-        + ", token expiration time=" + env.getProperty("jwt.token_expiration_time"));
+    return String.format(
+        "It's Working in User Service, port(local.server.port)=%s, port(server.port)=%s, token secret=%s, token expiration time=%s",
+        env.getProperty("local.server.port"),
+        env.getProperty("server.port"),
+        env.getProperty("jwt.secret"),
+        env.getProperty("jwt.token_expiration_time")
+    );
   }
 
+  /**
+   * 환영 메시지
+   */
   @GetMapping("/welcome")
   @Timed(value = "users.welcome", longTask = true)
   public String welcome() {
     return greeting;
   }
 
+  /**
+   * 신규 유저 생성
+   */
   @PostMapping("/users")
-  public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
-    ModelMapper mapper = new ModelMapper();
-    mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-    UserDto userDto = mapper.map(user, UserDto.class);
+  public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser requestUser) {
+    UserDto userDto = modelMapper.map(requestUser, UserDto.class);
     userService.createUser(userDto);
 
-    ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
-
+    ResponseUser responseUser = modelMapper.map(userDto, ResponseUser.class);
     return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
   }
 
-  @GetMapping("/user-service")
-  public void userTestingService(String testCode) {
-
-  }
-
+  /**
+   * 전체 유저 조회
+   */
   @GetMapping("/users")
   public ResponseEntity<List<ResponseUser>> getUsersAll() {
-    Iterable<UserEntity> userList = userService.getUserByAll();
+    List<ResponseUser> users = StreamSupport.stream(userService.getUserByAll().spliterator(), false)
+        .map(entity -> modelMapper.map(entity, ResponseUser.class))
+        .collect(Collectors.toList());
 
-    List<ResponseUser> resut = new ArrayList<>();
-    userList.forEach(v -> {
-      resut.add(new ModelMapper().map(v, ResponseUser.class));
-    });
-
-    return ResponseEntity.status(HttpStatus.OK).body(resut);
+    return ResponseEntity.ok(users);
   }
 
+  /**
+   * 특정 유저 조회
+   */
   @GetMapping("/users/{userId}")
-  public ResponseEntity<ResponseUser> getUsers(@PathVariable("userId") String userId) {
+  public ResponseEntity<ResponseUser> getUser(@PathVariable("userId") String userId) {
     UserDto userDto = userService.getUserByUserId(userId);
-
-    ResponseUser map = new ModelMapper().map(userDto, ResponseUser.class);
-
-    return ResponseEntity.status(HttpStatus.OK).body(map);
+    ResponseUser responseUser = modelMapper.map(userDto, ResponseUser.class);
+    return ResponseEntity.ok(responseUser);
   }
 
+  /**
+   * 테스트용 엔드포인트 (미사용)
+   */
+  @GetMapping("/user-service")
+  public void userTestingService(String testCode) {
+    // TODO: 필요 시 구현
+  }
 }
