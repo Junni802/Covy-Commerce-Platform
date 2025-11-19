625dadbaa5e720ae1bbf5875b89aa8fe92636b60
covy <junni802@naver.com>
2025-11-19 16:57:37 +0900

    refactor(user-dto): add builder pattern and comments for UserDto

--- covy-user/src/main/java/covy/covyuser/user/dto/UserDto.java
+++ covy-user/src/main/java/covy/covyuser/user/dto/UserDto.java
@@ -3,25 +3,30 @@ package covy.covyuser.user.dto;
 import covy.covyuser.user.vo.ResponseOrder;
 import java.util.Date;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * User DTO
+ * - 사용자 정보를 표현
+ * - 주문 정보까지 포함
+ */
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDto {
 
-  private String email;
-
-  private String pwd;
-
-  private String name;
-
-  private String userId;
-
-  private Date createAt;
-
-  private String encryptedPwd;
-
-  private List<ResponseOrder> orders;
+  private String email;             // 사용자 이메일
+  private String pwd;               // 원본 비밀번호
+  private String name;              // 사용자 이름
+  private String userId;            // 사용자 고유 ID
+  private Date createAt;            // 생성 일자
+  private String encryptedPwd;      // 암호화된 비밀번호
+  private List<ResponseOrder> orders; // 주문 내역 리스트
 
 }
