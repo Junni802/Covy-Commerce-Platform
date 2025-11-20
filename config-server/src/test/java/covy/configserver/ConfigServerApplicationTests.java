f4d624460f9622a2aa1a9a8881873a966b9252d7
covy <junni802@naver.com>
2025-11-20 21:42:42 +0900

    feat:Add Config server

---
+++ config-server/src/test/java/covy/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
