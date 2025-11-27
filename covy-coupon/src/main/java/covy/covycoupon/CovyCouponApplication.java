15d1f19921d3861a1106938bec144af10be137b2
covy <junni802@naver.com>
2025-11-27 18:51:58 +0900

    feat:Add covy-coupon project

---
+++ covy-coupon/src/main/java/covy/covycoupon/CovyCouponApplication.java
@@ -0,0 +1,13 @@
+package covy.covycoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovyCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovyCouponApplication.class, args);
+	}
+
+}
---
+++ covy-coupon/src/test/java/covy/covycoupon/CovyCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.covycoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovyCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
