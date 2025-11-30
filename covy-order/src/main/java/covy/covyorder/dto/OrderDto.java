3ffe2fc8215914c86d9a1ff52a5787db4967bc4c
covy <junni802@naver.com>
2025-11-30 19:42:15 +0900

    fix: change OrderDto
    - productId -> goodsCd Change name

--- covy-order/src/main/java/covy/covyorder/dto/OrderDto.java
+++ covy-order/src/main/java/covy/covyorder/dto/OrderDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class OrderDto {
 
-  private String productId;
+  private String goodsCd;
   private Integer qty;
   private Integer unitPrice;
   private Integer totalPrice;
