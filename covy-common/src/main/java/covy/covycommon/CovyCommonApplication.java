fc2170da26199c721d2508d320295f6ea0b79c4d
covy <junni802@naver.com>
2026-01-26 23:10:02 +0900

    feat: covy-common 프로젝트 생성

---
+++ covy-common/src/main/java/covy/covycommon/CovyCommonApplication.java
@@ -0,0 +1,13 @@
+package covy.covycommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovyCommonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CovyCommonApplication.class, args);
+  }
+
+}
---
+++ covy-common/src/test/java/covy/covycommon/CovyCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.covycommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovyCommonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
